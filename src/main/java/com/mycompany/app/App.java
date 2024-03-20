791111b40fe6991619dcc275aa3a2511081ef9cd
Saurabh Sharma <saurabhsharmapune@gmail.com>
2024-03-20 15:24:40 +0530

    test commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+		//test comment
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
