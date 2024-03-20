ef67c4491eda9384b273986ae4947b395729d0e1
Saurabh Sharma <saurabhsharmapune@gmail.com>
2024-03-20 15:27:41 +0530

    test commit2

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -14,6 +14,7 @@ public class AppTest
         App app1 = new App();
         App app2 = new App();
         assertEquals(app1.getMessage(), app2.getMessage());
+		//test comment
     }
 
     @Test
